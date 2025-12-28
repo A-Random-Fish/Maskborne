8244a6d15116839dc4e4cba28d9dea30f1dc545c
TheSnailL0rd <jacob.catninja@gmail.com>
2025-12-28 23:30:05 +0000

    Buttons kinda do stuff now

--- Assets/Scripts/ShopUpdates.cs
+++ Assets/Scripts/ShopUpdates.cs
@@ -5,9 +5,40 @@ using TMPro;
 public class ShopUpdates : MonoBehaviour
 {
     [SerializeField] TextMeshProUGUI soulsText;
+    
+    public int healthSoulsCost;
+    public int damageSoulsCost;
+    public int cooldownSoulsCost;
 
     void Update()
     {
         soulsText.text = StaticData.souls.ToString();
+    }
+
+    public void HealthUpgrade()
+    {
+        if (StaticData.souls >= healthSoulsCost)
+        {
+            StaticData.souls -= healthSoulsCost;
+            StaticData.healthIncrease += 0.25f;
+        }
+    }
+
+    public void DamageUpgrade()
+    {
+        if (StaticData.souls >= damageSoulsCost)
+        {
+            StaticData.souls -= damageSoulsCost;
+            StaticData.damageIncrease += 0.25f;
+        }
+    }
+
+    public void CooldownUpgrade()
+    {
+        if (StaticData.souls >= cooldownSoulsCost)
+        {
+            StaticData.souls -= cooldownSoulsCost;
+            StaticData.cooldownDecrease += 0.25f;
+        }
     }
 }
